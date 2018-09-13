50f013f9323a6f972cdce12d6fa7e8f9c596730e
rubenboot112 <ruben.boot@student.hu.nl>
2018-09-13 11:06:17 +0200

    les 4 oefenopdrachten

---
+++ pe4_1.py
@@ -0,0 +1,7 @@
+cijferICOR = 7.2
+cijferPROG = 7.4
+cijferCSN = 6.4
+
+gemiddelde = (cijferCSN + cijferPROG + cijferICOR)/3
+beloning = gemiddelde * 30
+print('Het gemiddelde cijfer voor de 3 cursussen is ' + str(gemiddelde) + '. Hiermee verdien je ' + str(beloning) + ' euro!')
---
+++ pe4_3.py
@@ -0,0 +1,5 @@
+uurloon = input('Wat verdien je per uur? ')
+aantaluur = input('Hoeveel uur heb je gewerkt? ')
+print(uurloon)
+print(aantaluur)
+print(str(aantaluur) + ' uur werken levert ' + str((float(uurloon) * float(aantaluur))) + ' euro op!')
